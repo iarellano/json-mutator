431611c841280d489c843eebfeb32824829df7ef
Isaias Arellano <Isaias Arellano Delgado>
2020-08-25 19:36:46 -0500

    Pulling nested properties working

---
+++ src/main/javascript/array-schema.js
@@ -0,0 +1,20 @@
+var mapArray_1 = {
+    person: {
+        type: 'array',
+        items: [
+            {
+                givenName: 'fisrtName'
+            },
+            {
+                surname: 'lastName'
+            }
+        ]
+    }
+};
+
+
+var mapArray_2 = {
+    ".": {
+
+    }
+}
--- src/main/javascript/transformer.js
+++ src/main/javascript/transformer.js
@@ -1,95 +1,126 @@
+
+
+var print = print || function(value) { console.log(value) };
+
+// var console = console || {
+//     log: function() { print(arguments) }
+// };
+
+var printJson = function(value) { console.log(JSON.stringify(value, null, 4)) };
+
 var source = {
     firstName: "Isaias",
     familyName: {
-        lastName: "Arellano"
+        lastName: "Arellano",
+        motherName: {
+            name: "Delgado"
+        }
     },
-    secondLastName: "Delgado"
+    secondLastName: "10.5"
 };
 
 var map = {
-    giveName: "fistName",
-    lastName: {
-        source: "familyName",
-        type: "object",
-        properties: {
-            surname: "lastName"
-        }
-    },
-    motherName: {
-        source: "secondLastName",
-        type: "string"
+    type: "object",
+    properties: {
+        giveName: "firstName",
+        surname: {
+            source: ["familyName", "lastName"]
+        },
+        motherName: {
+            source: [
+                "familyName", "motherName", "name"
+            ]
+        },
+        lastName: {
+            source: "familyName",
+            type: "object",
+            properties: {
+                surname: "lastName"
+            }
+        },
     }
 };
 
 console.log(JSON.stringify(transform(source, map), null, 4));
 
-function isSimpleType(mapping) {
-    return typeof mapping === 'string'
-        || !mapping.hasOwnProperty('type')
-        || (mapping.type !== 'object' && mapping.type !== 'array')
-}
 
-function getPropertyName(mapping) {
-    return typeof mapping === 'string' ? mapping : mapping.source;
-}
+function transform(srcObject, mapping) {
+    var pointer = arguments[2] || [srcObject];
+    print ("----------------start---------------");
+    print ("SOURCE -------------------------------");
+    printJson(srcObject);
+    print ("MAPPING -------------------------------");
+    printJson(mapping);
+    print ("POINTER -------------------------------");
+    printJson(pointer);
+    print ("----------------end---------------");
 
-function getSimpleValue(srcObject, mapping) {
-
-    if (typeof mapping === 'string' && srcObject.hasOwnProperty(mapping)) {
+    if (typeof mapping === "string") {
         return srcObject[mapping];
     }
-    if (mapping.type !== 'object' && mapping.type !== 'array') {
-        if (mapping.name && srcObject.hasOwnProperty(mapping.source)) {
+    if (!mapping.type || (mapping.type && mapping.type != 'object' && mapping.type != 'array')) {
+        if (typeof mapping.source === 'string' || Array.isArray(mapping.source) && mapping.source.length == 1) {
+            var source = Array.isArray(mapping.source) ? mapping.source[0] : mapping.source;
             if (mapping.type) {
-                return asType(srcObject[mapping.source], mapping.type);
+                return asType(srcObject[source], mapping.type);
+            } else {
+                return srcObject[source] ? srcObject[source] : null;
             }
-            srcObject[mapping.property];
+        } if (Array.isArray(mapping.source)) {
+            var value = null;
+            if (srcObject[mapping.source[0]]) {
+                var thisSource = mapping.source.shift();
+                pointer.push(srcObject[thisSource]);
+                value = transform(srcObject[thisSource], mapping, pointer);
+                // @TODO add debug function
+                // printJson(value);
+                pointer.pop();
+                mapping.source.unshift(thisSource);
+                return value;
+            }
+        } else {
+
         }
     }
-}
-
-function transform(srcObject, mappings) {
-    var object = {};
-    for (var targetProperty in mappings) {
-        var mapping = mappings[targetProperty];
-        if (isSimpleType(mapping)) {
-            var value = getSimpleValue(srcObject, mapping);
-            if (value !== undefined) {
-                object[targetProperty] = value;
+    if (mapping.type === 'object') {
+        if (!srcObject) return null;
+        if (mapping.properties) {
+            var object = {};
+            if (!Array.isArray(mapping.source)) {
+                if (!mapping.source) {
+                    var object = {};
+                    for (var property in mapping.properties) {
+                        object[property] = transform(srcObject, mapping.properties[property], pointer);
+                    }
+                } else if (srcObject[mapping.source]) {
+                    var object = {};
+                    pointer.push(srcObject[mapping.source]);
+                    for (var property in mapping.properties) {
+                        object[property] = transform(srcObject[mapping.source], mapping.properties[property], pointer);
+                    }
+                    pointer.pop();
+                }
+            } else {
+                if (mapping.source) {
+                    var source = mapping.source[0];
+                    if (srcObject[source]) {
+                        mapping.shift();
+                    }
+                    mapping.unshift(source);
+                }
             }
+            return object;
+        } else {
+            // @ Throw rule 11
         }
-
-        // if (srcObject.hasOwnProperty(property)) {
-        //     var  mapping = mappings[property];
-        //     if (typeof mapping === "string") {
-        //         if (mapping.length > 0) {
-        //             object[mapping] = srcObject[property];
-        //         } else {
-        //             throw new Error('Target property names should no be empty.')
-        //         }
-        //     } else if (typeof mapping === 'object' && !Array.isArray(mapping) && mapping) {
-        //         if (mapping.name &&  mapping.type !== "object" && mapping.type !== "array") {
-        //             if (mapping.type) {
-        //                 object[mapping.name] = asType(srcObject[property], mapping.type);
-        //             } else {
-        //                 object[mapping.name] = srcObject[property];
-        //             }
-        //         } else if (mapping.type === 'object') {
-        //             object[mapping.name] = transform(srcObject[property], mapping.properties);
-        //         }
-        //     }
-        // }
     }
-    return object;
 }
 
 function asType(value, type) {
     var val = null;
-    if (value === null) {
-        return value;
-    }
     switch (type) {
         case "string":
+            if (value === null) return null;
             if (typeof value === "string") {
                 val = value;
             } else if (typeof value === "object" || Array.isArray(value)) {
@@ -98,6 +129,7 @@ function asType(value, type) {
             val = '' + value;
             break;
         case "integer":
+            if (value === null) return null;
             if (typeof value === "object" || Array.isArray(value)) {
                 throw new Error("Only native values can be converted to integer.");
             }
@@ -107,6 +139,7 @@ function asType(value, type) {
             }
             break;
         case "number":
+            if (value === null) return null;
             if (typeof value === "object" || Array.isArray(value)) {
                 throw new Error("Only native values can be converted to number.");
             }
@@ -116,6 +149,7 @@ function asType(value, type) {
             }
             break;
         case "boolean":
+            if (value === null) return null;
             switch(value) {
                 case true:
                 case "true":
@@ -137,4 +171,29 @@ function asType(value, type) {
             throw new Error('Unknown data type ' + type + '.');
     }
     return val;
+}
+
+
+function isSimpleType(mapping) {
+    return typeof mapping === 'string'
+        || !mapping.hasOwnProperty('type')
+        || (mapping.type !== 'object' && mapping.type !== 'array')
+}
+
+function getSimpleValue(srcObject, mapping) {
+    if (typeof mapping === 'string' && srcObject.hasOwnProperty(mapping)) {
+        return srcObject[mapping];
+    }
+    if (!mapping.type) {
+        // @TODO Validate mapping.source exists
+        return srcObject[mapping.source];
+    }
+    if (mapping.type !== 'object' && mapping.type !== 'array') {
+        if (mapping.source && srcObject.hasOwnProperty(mapping.source)) {
+            if (mapping.type) {
+                return asType(srcObject[mapping.source], mapping.type);
+            }
+            srcObject[mapping.source];
+        }
+    }
 }
